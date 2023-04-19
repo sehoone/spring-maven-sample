af2fa8273e6a48bd528bc32173e1a69213adbe28
sehoon <sehoon@openobject.net>
2023-04-19 10:33:00 +0900

    init

---
+++ src/main/java/com/sehoon/springmavensample/SpringMavenSampleApplication.java
@@ -0,0 +1,13 @@
+package com.sehoon.springmavensample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMavenSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMavenSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sehoon/springmavensample/SpringMavenSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sehoon.springmavensample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMavenSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
