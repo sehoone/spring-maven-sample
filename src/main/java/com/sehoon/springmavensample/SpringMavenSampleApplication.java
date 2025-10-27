1188da7d091d40d3d14020c6eeccd49d4c04d056
sehoone.choi <sehoone.choi@multicampus.com>
2025-10-27 10:19:29 +0900

    hotfix/add-config-properties

--- src/main/java/com/sehoon/springmavensample/SpringMavenSampleApplication.java
+++ src/main/java/com/sehoon/springmavensample/SpringMavenSampleApplication.java
@@ -2,7 +2,11 @@ package com.sehoon.springmavensample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import com.sehoon.springmavensample.common.config.ApplicationProperties;
+
+@EnableConfigurationProperties(ApplicationProperties.class)
 @SpringBootApplication
 public class SpringMavenSampleApplication {
 
